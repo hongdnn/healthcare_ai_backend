9658d1629da4ff07a559c4e4f7adb2ca0ac4b2e9
secretlycharles <imcharlesmendez@gmail.com>
2025-10-25 00:00:20 -0700

    added login endpoint

--- src/main.py
+++ src/main.py
@@ -1,20 +1,56 @@
-import asyncio
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from livekit import agents
-from src.agent import entrypoint
+from pydantic import BaseModel
+from pymongo import AsyncMongoClient
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv(".env.local")
 
 # --- Create FastAPI app --- #
-app = FastAPI()
+app = FastAPI(
+    title="Healthcare AI API"
+)
+client = AsyncMongoClient(os.environ["MONGODB_CONNECTION_STRING"])
+db = client.healthcare_db
+user_collection = db.users
 
-# Index endpoint
 @app.get("/")
-async def root():
+async def index():
     return JSONResponse({"status": "ok", "message": "Healthcare assistant API running"})
 
-
-# Example health check endpoint
 @app.get("/health")
 async def health():
     return JSONResponse({"status": "healthy"})
+    
+class UserModel(BaseModel):
+    """
+    Container for a single user record
+    """
+    name: str
+    phone: str
+    email: str
+    type: str
+
+class LoginModel(BaseModel):
+    """
+    Container for login payload
+    """
+    email: str
+
+@app.post("/login")
+async def login(data: LoginModel):
+    print("user: ", data.email)
+    user = await user_collection.find_one({"email": data.email, "type": "doctor"})
+
+    if user is not None:
+        return JSONResponse(
+            status_code=200,
+            content={"status": "ok", "message": "Login successful", "id": str(user["_id"])}
+        )
+
+    return JSONResponse(
+        status_code=401,
+        content={"status": "failed", "message": "Login failed"}
+    )
